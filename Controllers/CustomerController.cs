9a4042aa195f5f9494138a163efb03bae6887521
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-02-01 19:48:21 +0600

    create mobile responsive banner

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
