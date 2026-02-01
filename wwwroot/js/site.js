ba5d4fd51f215cd321e2672db588288f0eeb8ded
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-02-01 12:34:44 +0600

    responsive the mobile navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// mobile side navbar function
+document.addEventListener("DOMContentLoaded", function () {
+    const openBtn = document.querySelector('.openIcon').parentElement;
+    const closeBtn = document.querySelector('.closeIcon').parentElement;
+    const sideBar = document.getElementById('sideBar');
+    const allLinks = document.querySelectorAll('#sideBar a');
 
-// Write your JavaScript code.
+    // Hide the close button at the beginning
+    closeBtn.style.display = 'none';
+
+    // Logic for opening the menu
+    openBtn.addEventListener('click', function () {
+        sideBar.classList.add('sidebar-active');
+        openBtn.style.display = 'none';
+        closeBtn.style.display = 'block';
+    });
+
+    // Menu close function
+    function closeMenu() {
+        sideBar.classList.remove('sidebar-active');
+        openBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+    }
+
+    closeBtn.addEventListener('click', closeMenu);
+
+    // Clicking on any link will close the sidebar
+    allLinks.forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+});
