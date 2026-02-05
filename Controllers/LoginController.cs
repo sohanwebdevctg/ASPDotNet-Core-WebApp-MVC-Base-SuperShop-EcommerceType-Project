c619cb7331da6e9138f71f8acd5b2f3a1806c8bd
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-02-05 16:35:46 +0600

    responsive the login form

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //registation
+        public IActionResult Registation()
+        {
+            return View();
+        }
+
+    }
+}
