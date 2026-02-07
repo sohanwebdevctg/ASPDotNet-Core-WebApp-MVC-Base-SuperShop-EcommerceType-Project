c9b2c2cae9fa73d1ef2021f8eb8327975757edf8
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-02-07 12:13:02 +0600

    creating common table in userdata show

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        // userdata
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
