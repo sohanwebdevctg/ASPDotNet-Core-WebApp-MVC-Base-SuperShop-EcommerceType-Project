a46ccd45f0e5aadf9ad51f913d4bef51c40a23c9
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-02-10 00:13:43 +0600

    create city models table

---
+++ Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        public string CityName { get; set; }
+
+    }
+}
